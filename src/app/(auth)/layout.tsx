8a8b62fd5d9ab3dfc61633bf04826cd7877b660f
Rico Erlangga <ricoerlanggaa@gmail.com>
2025-01-04 10:56:05 +0700

    add user register page

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,25 @@
+import type { ReactNode } from 'react';
+import Icon from '@mdi/react';
+import { mdiArrowLeft } from '@mdi/js';
+import { Button } from '@/components';
+
+export default function AuthLayout({
+  children,
+}: Readonly<{
+  children: ReactNode;
+}>) {
+  return (
+    <div className="grid md:grid-cols-5 h-screen bg-base-content">
+      <Button href="/" variant="ghost" className="absolute top-1 left-0">
+        <Icon path={mdiArrowLeft} size={1} className="text-base-content md:text-base-100" />
+      </Button>
+      <aside
+        className="hidden md:block md:col-span-2 bg-auto bg-no-repeat bg-bottom"
+        style={{ backgroundImage: 'url("/images/banners/polygon-wave.png")' }}
+      />
+      <main className="md:col-span-3 md:rounded-l-2xl bg-base-100  flex items-center justify-center">
+        {children}
+      </main>
+    </div>
+  );
+}
---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from 'next';
+import Link from 'next/link';
+import { FormRegister, Typography } from '@/components';
+
+export const metadata: Metadata = {
+  title: 'Lumea',
+};
+
+export default function Register() {
+  return (
+    <div className="card w-full sm:w-4/5 md:w-3/4 lg:w-1/2">
+      <div className="card-body">
+        <Typography as="h1" variant="h2" weight="semibold">
+          Daftar
+        </Typography>
+        <FormRegister />
+        <Typography className="text-center">
+          Already have an account?{' '}
+          <Link href="/login" className="hover:underline">
+            Login
+          </Link>
+        </Typography>
+      </div>
+    </div>
+  );
+}

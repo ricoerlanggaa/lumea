0b77852dc6f3925dc5aa8a33d75c880531e73985
Rico Erlangga <ricoerlanggaa@gmail.com>
2025-01-04 10:53:20 +0700

    add custom component input

---
+++ src/components/atoms/input/Input.tsx
@@ -0,0 +1,52 @@
+import { useId } from 'react';
+import { FieldError, FieldValues } from 'react-hook-form';
+import { classNames } from '@/utilities/formats/string';
+import type { InputProps } from './Input.d';
+
+export default function Input<TInputValues extends FieldValues>({
+  type = 'text',
+  label,
+  placeholder,
+  prependIcon,
+  appendIcon,
+  className,
+  inputKey,
+  register,
+  errors,
+  ...rest
+}: InputProps<TInputValues>) {
+  const inputId = useId();
+  const errorMessages = errors
+    ? (errors[inputKey as keyof typeof errors] as FieldError | undefined)
+    : undefined;
+  const hasError = !!(errors && errorMessages);
+  const classes = classNames(
+    'input input-bordered flex items-center gap-2',
+    hasError && 'input-error',
+    className,
+  );
+  return (
+    <div className="form-control">
+      {label && (
+        <label htmlFor={inputId} className="label">
+          <span className="label-text">{label}</span>
+        </label>
+      )}
+      <label className={classes}>
+        {prependIcon}
+        <input
+          id={inputId}
+          type={type}
+          placeholder={placeholder}
+          className="grow"
+          {...(register && register(inputKey))}
+          {...rest}
+        />
+        {appendIcon}
+      </label>
+      <label htmlFor={inputId} className="label">
+        {hasError && <span className="label-text-alt text-error">{errorMessages?.message}</span>}
+      </label>
+    </div>
+  );
+}

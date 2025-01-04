262e3a96b1bbcb2b27e198750778187f6b1c1370
Rico Erlangga <ricoerlanggaa@gmail.com>
2025-01-04 10:55:43 +0700

    add description banner home

--- src/app/(guest)/(home)/page.tsx
+++ src/app/(guest)/(home)/page.tsx
@@ -7,20 +7,27 @@ export default function Home() {
       style={{ backgroundImage: 'url("/images/banners/polygon-wave.png")' }}
     >
       <div className="hero-content text-base-100 text-center">
-        <div className="max-w-lg">
-          <Typography variant="h1" color="primary" weight="bold">
-            AI Customer Service
-          </Typography>
-          <Typography as="p" variant="h1" color="secondary" weight="bold">
+        <div className="max-w-sm md:max-w-md lg:max-w-lg">
+          <Typography variant="h1">
+            <Typography as="strong" color="primary">
+              AI Customer Service
+            </Typography>
+            <br />
             Handle Ribuan Leads
-          </Typography>
-          <Typography as="p" variant="h1" color="secondary" weight="bold" className="mb-6">
+            <br />
             Hingga Closing
           </Typography>
-          <Button variant="outlined" color="primary" className="mr-2">
+          <Typography variant="body" className="my-4">
+            <Typography as="strong" color="primary">
+              Lumea AI
+            </Typography>{' '}
+            merupakan platform Customer Service berbasis AI yang bisa bantu handle leads dan chat
+            yang masuk hingga closing dengan cepat dan mudah
+          </Typography>
+          <Button href="/register" variant="outlined" color="primary" className="mr-2">
             Daftar Sekarang
           </Button>
-          <Button variant="outlined" color="white">
+          <Button href="/about" variant="outlined" color="white">
             Pelajari Selengkapnya
           </Button>
         </div>

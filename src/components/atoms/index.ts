4f7c292be63004b038b40d8419e0ce0e8180274d
Rico Erlangga <ricoerlanggaa@gmail.com>
2025-01-04 17:59:27 +0700

    add custom component select

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -2,3 +2,4 @@ export { default as Button } from './button/Button';
 export { default as Typography } from './typography/Typography';
 export { default as Input } from './input/Input';
 export { default as TextArea } from './textarea/TextArea';
+export { default as Select } from './select/Select';
---
+++ src/components/atoms/select/Select.d.ts
@@ -0,0 +1,17 @@
+import { SelectHTMLAttributes } from 'react';
+import { DeepMap, FieldError, FieldValues, Path, UseFormReturn } from 'react-hook-form';
+
+type SelectItems = {
+  key: number | string;
+  label: string;
+  value: string;
+}[];
+
+interface SelectProps<TInputValues extends FieldValues>
+  extends Partial<UseFormReturn<TInputValues>>,
+    SelectHTMLAttributes<HTMLElement> {
+  label?: string;
+  items?: SelectItems;
+  inputKey: Path<TInputValues>;
+  errors?: Partial<DeepMap<TInputValues, FieldError>>;
+}

22950d3a4be97b41697c1e182610a3ec0d5b14af
ricoerlanggaa <ricoerlanggaa@gmail.com>
2025-01-17 22:47:48 +0700

    update ai customer service page

--- src/app/(user)/product-setup/ai-customer-service/page.tsx
+++ src/app/(user)/product-setup/ai-customer-service/page.tsx
@@ -1,28 +1,46 @@
 import Link from 'next/link';
 import Icon from '@mdi/react';
 import { mdiPlus } from '@mdi/js';
-import { Typography } from '@/components/atoms';
+import { Button, Typography } from '@/components/atoms';
+import { Breadcrumbs } from '@/components/molecules';
 
+const breadcrumbsItems = [
+  { key: 1, label: 'Product Setup' },
+  { key: 2, label: 'AI Customer Service' },
+];
 export default function AiCustomerService() {
   return (
-    <div className="card h-full overflow-y-auto bg-base-100">
-      <div className="card-body">
-        <Typography as="h1" variant="h3" weight="bold" className="card-title">
-          AI Customer Service
-        </Typography>
-        <div className="divider my-1" />
-        <Typography as="h2" variant="h4" weight="normal" className="card-title mb-4">
-          Daftar CS AI
-        </Typography>
-        <div className="grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8">
-          <Link
-            href="/product-setup/ai-customer-service/add"
-            className="card h-32 max-w-32 bg-base-200 border-primary hover:cursor-pointer items-center border-2 border-dashed"
-          >
-            <Icon path={mdiPlus} size={2} className="my-auto" />
-          </Link>
+    <>
+      <Breadcrumbs items={breadcrumbsItems} />
+      <div className="h-full flex flex-col gap-4 overflow-y-auto">
+        <div className="card bg-base-100">
+          <div className="card-body">
+            <Typography as="h1" variant="h4" className="card-title mb-2">
+              Daftar CS AI
+            </Typography>
+            <hr className="mb-4" />
+            <div className="grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8">
+              <Link
+                href="/product-setup/ai-customer-service/add"
+                className="card h-32 max-w-28 bg-base-200 border-primary hover:cursor-pointer items-center border-2 border-dashed"
+              >
+                <Icon path={mdiPlus} size={2} className="my-auto" />
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="card bg-base-100">
+          <div className="card-body">
+            <Typography as="h1" variant="h4" className="card-title mb-2">
+              Integrasi Nomor Whatsapp
+            </Typography>
+            <hr className="mb-4" />
+            <Button color="black" className="mr-auto">
+              Hubungkan Nomor
+            </Button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }

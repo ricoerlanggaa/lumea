d1151e39590d9fd2b31b04c64c9fb1b367a43004
ricoerlanggaa <ricoerlanggaa@gmail.com>
2025-01-20 04:04:13 +0700

    update whatsapp list table

--- src/components/templates/tables/WhatsappTable.tsx
+++ src/components/templates/tables/WhatsappTable.tsx
@@ -96,36 +96,38 @@ export default function WhatsappTable({ items }: { items: WhatsappItem[] }) {
   ];
 
   return (
-    <table className="table border">
-      <thead>
-        <tr>
-          <th>Nomor Whatsapp</th>
-          <th>Status</th>
-          <th aria-label="aksi" />
-        </tr>
-      </thead>
-      <tbody>
-        {whatsappList?.map((item) => (
-          <tr key={item.id}>
-            <td>{formatPhoneNumber(item.number)}</td>
-            <td>
-              <div
-                className={classNames(
-                  'badge badge-outline',
-                  item.isConnected ? 'badge-success' : 'badge-error',
-                )}
-              >
-                {item.isConnected ? 'Connected' : 'Disconnected'}
-              </div>
-            </td>
-            <td className="text-right">
-              <Dropdown placement="bottom-end" menuItems={generateMenuItems(item)} menuSize="sm">
-                <MoreHorizontalCircle01Icon size={16} className="ml-auto" />
-              </Dropdown>
-            </td>
+    <div className="overflow-x-auto w-full">
+      <table className="table">
+        <thead>
+          <tr className="bg-base-200">
+            <th className="border">Nomor Whatsapp</th>
+            <th className="border">Status</th>
+            <th className="border text-center">Aksi</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {whatsappList?.map((item) => (
+            <tr key={item.id}>
+              <td className="border">{formatPhoneNumber(item.number)}</td>
+              <td className="border">
+                <div
+                  className={classNames(
+                    'badge badge-outline',
+                    item.isConnected ? 'badge-success' : 'badge-error',
+                  )}
+                >
+                  {item.isConnected ? 'Connected' : 'Disconnected'}
+                </div>
+              </td>
+              <td className="border text-center">
+                <Dropdown placement="bottom-end" menuItems={generateMenuItems(item)} menuSize="sm">
+                  <MoreHorizontalCircle01Icon size={16} />
+                </Dropdown>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }

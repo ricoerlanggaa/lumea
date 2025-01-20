af86f8f5a20c0c454dd30d6e46caeda09c07de5e
ricoerlanggaa <ricoerlanggaa@gmail.com>
2025-01-20 09:15:19 +0700

    fix access token

--- src/utilities/http/apiClient.ts
+++ src/utilities/http/apiClient.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import Cookies from 'js-cookie';
+import { cookies } from 'next/headers';
 
 const BASE_URL = `${process.env.NEXT_PUBLIC_API_URL}/api/v1`;
 
@@ -14,9 +14,10 @@ const apiClient = axios.create({
 apiClient.interceptors.request.use(
   async (config) => {
     const custom = config;
-    const accessToken = Cookies.get('access_token');
-    if (accessToken) {
-      custom.headers.Authorization = `Bearer ${accessToken}`;
+    const cookieStore = await cookies();
+    const accessToken = cookieStore.get('access_token');
+    if (accessToken?.value) {
+      custom.headers.Authorization = `Bearer ${accessToken.value}`;
     }
     return custom;
   },
@@ -27,22 +28,23 @@ apiClient.interceptors.response.use(
   (response) => response,
   async (error) => {
     const originalRequest = error.config || {};
-    const refreshToken = Cookies.get('refresh_token');
+    const cookieStore = await cookies();
+    const refreshToken = cookieStore.get('refresh_token');
     if (error.response?.status === 401 && refreshToken) {
       try {
         const response = await axios.get(`${BASE_URL}/user/generate-access-token`, {
-          headers: { Authorization: `Bearer ${refreshToken}` },
+          headers: { Authorization: `Bearer ${refreshToken.value}` },
           timeout: 10000,
         });
         const { data } = response.data;
         if (data.access_token && data.refresh_token) {
-          Cookies.set('access_token', data.access_token);
-          Cookies.set('refresh_token', data.refresh_token);
+          cookieStore.set('access_token', data.access_token);
+          cookieStore.set('refresh_token', data.refresh_token);
           return apiClient(originalRequest);
         }
       } catch (refreshError) {
-        Cookies.remove('access_token');
-        Cookies.remove('refresh_token');
+        cookieStore.delete('access_token');
+        cookieStore.delete('refresh_token');
         return Promise.reject(refreshError);
       }
     }
